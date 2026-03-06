d542bf785d43867c7fa1e99f282a7a82d1378399
dd114 <my_email@mail.com>
2026-03-06 20:50:13 +0300

    feat(code instrumentation): basic functionality of 'alloca', 'store' and 'load' params printing has been implemented

--- CodeInstrumentation/CodeInstrumentation.cpp
+++ CodeInstrumentation/CodeInstrumentation.cpp
@@ -3,77 +3,255 @@
 #include "llvm/IR/PassManager.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/DataLayout.h"
-#include "llvm/Analysis/MemorySSA.h"
-#include "llvm/Analysis/MemorySSAUpdater.h"
-#include "llvm/Analysis/AliasAnalysis.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Verifier.h"
 #include "llvm/Support/raw_ostream.h"
+
+#include <vector>
 
 using namespace llvm;
 
 namespace {
 
-void printInstrumentation(Function &F, FunctionAnalysisManager &FAM) {
-  const DataLayout &DL = F.getParent()->getDataLayout();
+struct MemoryOpInfo {
+    Instruction *Inst;
 
-  outs() << "\n=============================\n";
-  outs() << "Function: " << F.getName()
-         << "; Number of arguments: " << F.arg_size() << "\n";
-  outs() << "=============================\n";
+    enum OpType {
+        LOAD,
+        STORE,
+        ALLOCA
+    } Type;
 
-  for (auto &BB : F) {
-      for (auto &Inst : BB) {
-
-
-
-        //   outs() << "---------------------------\n";
-      }
-  }
-}
-
-// New PM implementation
-struct CodeInstrumentation : PassInfoMixin<CodeInstrumentation> {
-  // Main entry point, takes IR unit to run the pass on (&F) and the
-  // corresponding pass manager (to be queried if need be)
-  PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM) {
-    // visitor(F);
-
-
-    printInstrumentation(F, FAM);
-
-    return PreservedAnalyses::all();
-  }
-
-  // Without isRequired returning true, this pass will be skipped for functions
-  // decorated with the optnone LLVM attribute. Note that clang -O0 decorates
-  // all functions with optnone.
-  static bool isRequired() { return true; }
+    Value *Pointer;
+    llvm::Type *ValueType;
+    uint64_t Size;
+    uint64_t Align;
 };
-} // namespace
 
+GlobalVariable *getOrCreateFormat(Module *M,
+                                  const std::string &Format,
+                                  const std::string &Name) {
 
+    if (auto *G = M->getNamedGlobal(Name))
+        return G;
 
-//-----------------------------------------------------------------------------
-// New PM Registration
-//-----------------------------------------------------------------------------
-llvm::PassPluginLibraryInfo getMyPassPluginInfo() {
-  return {LLVM_PLUGIN_API_VERSION, "CodeInstrumentation", LLVM_VERSION_STRING,
-          [](PassBuilder &PB) {
-            PB.registerPipelineParsingCallback(
-                [](StringRef Name, FunctionPassManager &FPM,
-                   ArrayRef<PassBuilder::PipelineElement>) {
-                  if (Name == "CodeInstrumentation") {
-                    FPM.addPass(CodeInstrumentation());
-                    return true;
-                  }
-                  return false;
-                });
-          }};
+    LLVMContext &Ctx = M->getContext();
+
+    Constant *Str = ConstantDataArray::getString(Ctx, Format);
+
+    auto *GV = new GlobalVariable(
+        *M,
+        Str->getType(),
+        true,
+        GlobalValue::PrivateLinkage,
+        Str,
+        Name
+    );
+
+    GV->setUnnamedAddr(GlobalValue::UnnamedAddr::Global);
+
+    return GV;
 }
 
-// This is the core interface for pass plugins. It guarantees that 'opt' will
-// be able to recognize HelloWorld when added to the pass pipeline on the
-// command line, i.e. via '-passes=hello-world'
-extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
-llvmGetPassPluginInfo() {
-  return getMyPassPluginInfo();
+Value *getStringPtr(IRBuilder<> &Builder, GlobalVariable *GV) {
+
+    LLVMContext &Ctx = Builder.getContext();
+
+    Value *Zero = ConstantInt::get(Type::getInt32Ty(Ctx), 0);
+
+    return Builder.CreateInBoundsGEP(
+        GV->getValueType(),
+        GV,
+        {Zero, Zero}
+    );
+}
+
+void instrumentFunction(Function &F) {
+
+    Module *M = F.getParent();
+    LLVMContext &Ctx = M->getContext();
+    const DataLayout &DL = M->getDataLayout();
+
+    outs() << "\n=== Function: " << F.getName() << " ===\n";
+
+    Function *Printf = M->getFunction("printf");
+
+    if (!Printf) {
+
+        FunctionType *PrintfTy =
+            FunctionType::get(
+                Type::getInt32Ty(Ctx),
+                {PointerType::getUnqual(Ctx)},
+                true
+            );
+
+        Printf = Function::Create(
+            PrintfTy,
+            GlobalValue::ExternalLinkage,
+            "printf",
+            M
+        );
+    }
+
+    auto *LoadFmt = getOrCreateFormat(
+        M,
+        "LOAD addr=%p size=%lu align=%lu\n",
+        "log_load_fmt"
+    );
+
+    auto *StoreFmt = getOrCreateFormat(
+        M,
+        "STORE addr=%p size=%lu align=%lu\n",
+        "log_store_fmt"
+    );
+
+    auto *AllocaFmt = getOrCreateFormat(
+        M,
+        "ALLOCA addr=%p size=%lu align=%lu\n",
+        "log_alloca_fmt"
+    );
+
+    std::vector<MemoryOpInfo> Ops;
+
+    for (auto &BB : F) {
+        for (auto &I : BB) {
+
+            MemoryOpInfo Info;
+
+            if (auto *LI = dyn_cast<LoadInst>(&I)) {
+
+                Info.Inst = LI;
+                Info.Type = MemoryOpInfo::LOAD;
+                Info.Pointer = LI->getPointerOperand();
+                Info.ValueType = LI->getType();
+                Info.Size = DL.getTypeStoreSize(Info.ValueType);
+                Info.Align = LI->getAlign().value();
+
+                Ops.push_back(Info);
+            }
+
+            else if (auto *SI = dyn_cast<StoreInst>(&I)) {
+
+                Info.Inst = SI;
+                Info.Type = MemoryOpInfo::STORE;
+                Info.Pointer = SI->getPointerOperand();
+                Info.ValueType = SI->getValueOperand()->getType();
+                Info.Size = DL.getTypeStoreSize(Info.ValueType);
+                Info.Align = SI->getAlign().value();
+
+                Ops.push_back(Info);
+            }
+
+            else if (auto *AI = dyn_cast<AllocaInst>(&I)) {
+
+                Info.Inst = AI;
+                Info.Type = MemoryOpInfo::ALLOCA;
+                Info.Pointer = AI;
+                Info.ValueType = AI->getAllocatedType();
+                Info.Size = DL.getTypeAllocSize(Info.ValueType);
+                Info.Align = AI->getAlign().value();
+
+                Ops.push_back(Info);
+            }
+        }
+    }
+
+    outs() << "Memory ops found: " << Ops.size() << "\n";
+
+    for (auto &Info : Ops) {
+
+        Instruction *InsertPoint = Info.Inst;
+
+        if (Info.Type == MemoryOpInfo::STORE || Info.Type == MemoryOpInfo::ALLOCA || Info.Type == MemoryOpInfo::LOAD) { // for any cases now
+
+            if (Instruction *Next = Info.Inst->getNextNode())
+                InsertPoint = Next;
+        }
+
+        IRBuilder<> Builder(InsertPoint);
+
+        GlobalVariable *FmtGV =
+            Info.Type == MemoryOpInfo::LOAD  ? LoadFmt :
+            Info.Type == MemoryOpInfo::STORE ? StoreFmt :
+                                               AllocaFmt;
+
+        Value *FmtPtr = getStringPtr(Builder, FmtGV);
+
+        Value *PtrCast =
+            Builder.CreatePointerCast(
+                Info.Pointer,
+                PointerType::getUnqual(Ctx)
+            );
+
+        Value *SizeVal =
+            ConstantInt::get(
+                Type::getInt64Ty(Ctx),
+                Info.Size
+            );
+
+        Value *AlignVal =
+            ConstantInt::get(
+                Type::getInt64Ty(Ctx),
+                Info.Align
+            );
+
+        Builder.CreateCall(
+            Printf,
+            {FmtPtr, PtrCast, SizeVal, AlignVal}
+        );
+    }
+
+    verifyFunction(F, &errs());
+}
+
+struct CodeInstrumentation :
+    PassInfoMixin<CodeInstrumentation> {
+
+    PreservedAnalyses run(Function &F,
+                          FunctionAnalysisManager &) {
+
+        instrumentFunction(F);
+
+        return PreservedAnalyses::none();
+    }
+
+    static bool isRequired() { return true; }
+};
+
+} 
+
+PassPluginLibraryInfo getMyPassPluginInfo() {
+
+    return {
+        LLVM_PLUGIN_API_VERSION,
+        "CodeInstrumentation",
+        LLVM_VERSION_STRING,
+
+        [](PassBuilder &PB) {
+
+            PB.registerPipelineParsingCallback(
+
+                [](StringRef Name,
+                   FunctionPassManager &FPM,
+                   ArrayRef<PassBuilder::PipelineElement>) {
+
+                    if (Name == "CodeInstrumentation") {
+
+                        FPM.addPass(CodeInstrumentation());
+                        return true;
+                    }
+
+                    return false;
+                });
+        }
+    };
+}
+
+extern "C"
+LLVM_ATTRIBUTE_WEAK
+PassPluginLibraryInfo llvmGetPassPluginInfo() {
+
+    return getMyPassPluginInfo();
 }
